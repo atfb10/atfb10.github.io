c95cb67c42e1362e190ba36ab022c66bdc42ded2
atfb84 <atfb84@mst.edu>
2023-06-26 15:34:23 -0700

    Requirements file

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 '''
 Adam Forestier
-June 23, 2023
-
+June 26, 2023
+Automatically Create Blog Posts & post to my github.io page
 '''
 
-import openai
+# D:\coding\udemy\openai_blog_app
 
+
+# Imports
+import openai
 from key import key as openai_key
 
+# Set api key
 openai.api_key = openai_key

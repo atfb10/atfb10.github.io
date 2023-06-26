51f5f21b3787c11e03cbdef04a3d58b79d66add5
atfb84 <atfb84@mst.edu>
2023-06-26 15:22:29 -0700

    test landing page

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# Automatic Blog Post Mini Project
+* Auto-Generate blog posts on my github.io page using the OpenAI API
---
+++ app.py
@@ -0,0 +1,11 @@
+'''
+Adam Forestier
+June 23, 2023
+
+'''
+
+import openai
+
+from key import key as openai_key
+
+openai.api_key = openai_key
